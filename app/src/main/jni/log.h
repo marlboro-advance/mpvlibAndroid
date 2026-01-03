3532a65a6cfdc88628622b518e4b17ec952f50cc
MarlboroAdvance <marlboroadvance@zohomail.in>
2026-01-03 14:31:10 +0530

    Fix: Add ALOGW macro to log.h for thumbnail_fast.cpp

--- app/src/main/jni/log.h
+++ app/src/main/jni/log.h
@@ -6,6 +6,7 @@
 
 #define LOG_TAG "mpv"
 #define ALOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#define ALOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
 #if DEBUG
 #define ALOGV(...) __android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, __VA_ARGS__)
 #else
